c1dc50ab326e5d1fbff68fa94e0475a922837ba8
tempname1533 <pedro_de_sousa@outlook.com>
2020-11-18 10:29:50 +0100

    Updates

---
+++ precache-manifest.bbb3ed4ec54e1acfbe9b3d3f516346d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3384b32ee3dd5c51bd4773de0ca7cbce",
+    "url": "/React-music/index.html"
+  },
+  {
+    "revision": "18ce968fc4d0a22cc2cc",
+    "url": "/React-music/static/css/main.8789084e.chunk.css"
+  },
+  {
+    "revision": "07552c801face94b0284",
+    "url": "/React-music/static/js/2.36ef7f39.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/React-music/static/js/2.36ef7f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ce968fc4d0a22cc2cc",
+    "url": "/React-music/static/js/main.8cbe7b93.chunk.js"
+  },
+  {
+    "revision": "3d023367d6486b573f72",
+    "url": "/React-music/static/js/runtime-main.8289611a.js"
+  }
+]);
